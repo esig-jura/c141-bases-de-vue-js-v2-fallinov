b6b95705fcf7a8143c7ab85389e4f1321e018770
Fallet Steve <steve.fallet@divtec.ch>
2025-02-19 10:02:28 +0100

    ajout événement delete

--- src/components/PokemonCard.vue
+++ src/components/PokemonCard.vue
@@ -1,8 +1,39 @@
 <template>
-  <p>Coucou</p>
+  <v-list-item>
+    <v-list-item-title>
+      {{ pokeNom }}
+    </v-list-item-title>
+
+    <template v-slot:append>
+      <v-btn
+        icon="mdi-delete"
+        variant="text"
+        @click="deletePokemon"
+      ></v-btn>
+    </template>
+  </v-list-item>
 </template>
 
 <script setup>
+// Création de la propriété pokeNom
+defineProps({
+  pokeNom: {
+    type: String,
+    required: true,
+  },
+})
+
+// Déclaration de l'événement delete
+const emit = defineEmits(['delete'])
+
+// Fonction qui émet l'événement delete
+function deletePokemon () {
+  // Si l'utilisateur confirme la suppression
+  if(confirm('Voulez-vous vraiment supprimer ce Pokémon ?')) {
+    // Émission de l'événement delete
+    emit('delete')
+  }
+}
 
 </script>
 
--- src/pages/Exercice8.vue
+++ src/pages/Exercice8.vue
@@ -5,24 +5,19 @@
     <!-- Zone de travail pour l'exercice -->
     <div class="exe-zone">
       <h2>Zone d'exercice</h2>
+
       <!-- Liste des Pokémon -->
       <v-card class="mx-auto my-6 pa-2" max-width="500">
         <v-card-title>Liste des Pokémon</v-card-title>
         <v-card-text>
           <v-list>
             <!-- Element à transformer en composant -->
-            <v-list-item>
-              <v-list-item-title>
-                *** Pika Pika ! ***
-              </v-list-item-title>
-
-              <template v-slot:append>
-                <v-btn
-                  icon="mdi-delete"
-                  variant="text"
-                ></v-btn>
-              </template>
-            </v-list-item>
+            <pokemon-card
+              v-for="pokeball in pokemons"
+              :key="pokeball"
+              :poke-nom="pokeball"
+              @delete="removePokemon(0)"
+            />
             <!-- Fin de l'élément à transformer en composant -->
           </v-list>
         </v-card-text>
@@ -34,6 +29,7 @@
 <script setup>
 import { ref } from 'vue';
 import ExerciceObjectifs from "@/components/ExerciceObjectifs.vue";
+import PokemonCard from "@/components/PokemonCard.vue";
 
 // Liste initiale des Pokémon
 const pokemons = ref(["Pikachu", "Bulbizarre", "Salamèche", "Carapuce", "Rondoudou"]);
